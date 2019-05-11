d643d2624d18b8f98d6ec0346161c0b632b209c6
Robbie Martinus <12573669+rmartinus@users.noreply.github.com>
2019-05-11 12:12:43 +1000

    change dir name

--- pkg/gitlab/clone.go
+++ pkg/gitlab/clone.go
@@ -11,7 +11,7 @@ import (
 // Clone clones the given url to the current directory
 func Clone(name string, url string, token string) error {
 	fmt.Printf("Cloning %s to %s\n", url, name)
-	_, err := git.PlainClone("/tmp/test/"+name, false, &git.CloneOptions{
+	_, err := git.PlainClone("/tmp/clone-all/"+name, false, &git.CloneOptions{
 		URL: url,
 		Auth: &http.BasicAuth{
 			Password: token,
